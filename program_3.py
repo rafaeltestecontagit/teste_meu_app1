1acffed2ea67f4597a0b11625b1ee1d166685a91
Rafael Cardoso <rafael.teste.conta.git@tutanota.com>
2018-08-23 17:41:33 -0300

    new file:   program_3.py 	new file:   program_4.py 	new file:   program_5.py

---
+++ program_3.py
@@ -0,0 +1,24 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+nl = 200
+nc = 200
+matriz = list(range(0,nl,1))
+
+for i in range(0,nl,1):       
+    matriz[i] = list(range(0,nc,1))  
+
+
+
+
+
+
+
+
+
+
+
+
+
+
---
+++ program_5.py
@@ -0,0 +1,26 @@
+
+import matplotlib.pyplot as plt
+import numpy as np
+
+z = []
+matriz = list(range(0,nl,1))
+
+for i in range(0,nl,1):       
+    matriz[i] = list(range(0,nc,1))  
+
+for i in range(0,nl,1):
+    for j in range(0,nc,1):
+        matriz[i][j] = 10*np.exp(-((i-nl/2)/(0.2*nl))**2)+10*np.exp(-((j-nc/2)/(0.7*nc))**2)
+
+
+
+
+
+
+
+
+
+
+
+
+
